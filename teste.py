7cc71489c3a4d0c48bc7b930fecf3b4970fe2596
vinixxyama <vini_yamamoto@hotmail.com>
2018-05-19 15:55:10 -0300

    mudanca nos nomes das variaveis, trocado for por while, adicionado looping para ir ate o final da paginacao

--- teste.py
+++ teste.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
-def extrai_info():
+def extract_info():
     #Extrai as informacoes da pagina
     l1 = tree.xpath('//tr//td[1]/text()[normalize-space()]')
     l2 = tree.xpath('//tr//td//small[1]/text()[normalize-space()]')
@@ -20,17 +20,17 @@ def extrai_info():
         l2[i] = l2[i].encode('utf-8').strip()
         l3[i] = l3[i].encode('utf-8').strip()
         l5[i] = l5[i].encode('utf-8').strip()
-    listas(l1, l2, l3, l4, l5)
+    lists(l1, l2, l3, l4, l5)
 
-def listas(l1, l2, l3, l4, l5):
+def lists(l1, l2, l3, l4, l5):
     j = 0
     teste = 'D'
     #se for diferente de Dia nao adiciona
     for i in range(0, len(l5)):
         if l5[i] == teste:
-            produto.append(l1[i])
+            product.append(l1[i])
             ce.append(l2[i])
-            preco.append(l3[i])
+            price.append(l3[i])
             data.append(l4[i])
             freq.append(l5[i])
 
@@ -45,27 +45,33 @@ driver.get("https://www.agrolink.com.br/cotacoes/"+sit+"")
 #driver.find_element_by_xpath('//tr//td//a[@href="/cotacoes/graos/'+ sit +'"]').click()
 page = requests.get(driver.current_url)
 tree = html.fromstring(page.content)
-produto = []
+product = []
 ce = []
-preco = []
+price = []
 data = []
 freq = []
 
-extrai_info()
-for aux in range(160, 162):
+extract_info()
+walk = 0
+aux = 2
+while (walk == 0):
     p = str(aux)
-    #Executa o javascript da paginacao e vai para a proxima pagina
+    #Executa o javascript da paginacao e vai para a proxima pagina.
     driver.execute_script("javascript:navigateToPage('frmFiltroGeral-5231', " + p + ")")
-    test = tree.xpath('//*[@id="frmMercadoFisico-5181"]/div/span/text()')
-    if len(test) == 0:
-        print 'null'
-    time.sleep(5)
-    #ATUALIZA O HTML
-    page = driver.execute_script("return document.body.innerHTML")
-    tree = html.fromstring(page)
-    extrai_info()
+    #verifica se ja passou da ultima pagina.
+    last_page = tree.xpath('//*[@id="frmMercadoFisico-5181"]/div/span/text()')
+    #para o loop
+    if len(last_page) != 0:
+        walk = 1
+    else:
+        time.sleep(5)
+        #ATUALIZA O HTML
+        page = driver.execute_script("return document.body.innerHTML")
+        tree = html.fromstring(page)
+        extract_info()
+        aux = aux + 1
 
-row = zip(produto, ce, preco, data, freq)
+row = zip(product, ce, price, data, freq)
 #SALVA OS VALORES RECEBIDO EM UMA LISTA
 with open("csvfile", "w") as output:
     writer = csv.writer(output, lineterminator='\n')
